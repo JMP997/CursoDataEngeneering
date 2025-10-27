85d1e7589bd77d50254672f2637dbe399edd2351
MPJesús <mpjesus00@gmail.com>
2025-10-27 17:42:02 +0100

    Logica para conseguir los ID de los videos generada

--- video_stats.py
+++ video_stats.py
@@ -9,6 +9,7 @@ load_dotenv(dotenv_path="./.env")
 
 API_KEY = os.getenv("API_KEY")
 CHANNEL_HANDLE = "MrBeast"
+maxResults = 50
 
 # Funcion que estrae la ID de playlist de un canal de youtube a traves de un JSON
 def getPlaylistid():
@@ -27,7 +28,40 @@ def getPlaylistid():
 
         return channel_playlistID
     except requests.exceptions.RequestException as e:
+        raise e    
+
+# Esta función va a coger cada ID correspondiente a un video y la va a almacenar en una lista
+def getVideoIds(playlistId):
+    videoID = []
+    pageToken = None
+    baseURL = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlistId}&key={API_KEY}"
+# Mientras que la condición sea cuerta a la URL base se le va a añadir el pageToken
+    try:
+        while True:
+            url = baseURL
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+
+            for item in data.get('items', []):
+                video_id = item['contentDetails']['videoId']
+                videoID.append(video_id)
+
+            pageToken = data.get('nextPageToken')
+
+            with open("videoIds.json", "w", encoding="utf-8") as f:
+                json.dump(data, f, indent=4, ensure_ascii=False)
+
+            if not pageToken:
+                break
+
+        return videoID
+        
+    except requests.exceptions.RequestException as e:
         raise e
-    
+
 if __name__ == "__main__":
-    getPlaylistid()
+    playlistId = getPlaylistid()
+    print(getVideoIds(playlistId))
