f1e40dcd52d918776f5c1b280fce4cff2c1cbf20
MPJesús <mpjesus00@gmail.com>
2025-10-27 16:22:11 +0100

    Añadido return a función

--- video_stats.py
+++ video_stats.py
@@ -14,23 +14,18 @@ CHANNEL_HANDLE = "MrBeast"
 def getPlaylistid():
     try:
         url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
-
         response = requests.get(url)
-
         response.raise_for_status()
-
         data = response.json()
 
         # print(json.dumps(data, indent= 4))
 
         # Lo que estamos haciendo ahora es acceder a la playlist del canal mediante la clave del json que hemos obtenido desde la API
         channel_items = data["items"][0]
-
         channel_playlistID = channel_items["contentDetails"]["relatedPlaylists"]['uploads']
+        #print (channel_playlistID);
 
-        print (channel_playlistID);
-
-        # return channel_playlistID
+        return channel_playlistID
     except requests.exceptions.RequestException as e:
         raise e
     
